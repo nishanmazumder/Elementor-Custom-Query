d925fb351e5be3589f851356363d08c77d8fb166
Md Rashidul Azam (Nishan) <arosh019@gmail.com>
2024-05-12 20:20:54 +0600

    post no it updated

--- function.php
+++ function.php
@@ -1,30 +1,29 @@
 <?php
 add_action( 'elementor/query/display_related_product_by_category', function( $query ) {
-	
 	$queried_object = get_queried_object();
 	
 	if (isset($queried_object->ID) && $queried_object->post_type == 'product') {
-        $post_id = $queried_object->ID;
-        $categories = get_the_terms($post_id, 'product_category');
-
-        $category_ids = array();
-        foreach ($categories as $category) {
-            $category_ids[] = $category->term_id;
-        }
-		
+	        $post_id = $queried_object->ID;
+	        $categories = get_the_terms($post_id, 'product_category');
+	
+	        $category_ids = array();
+	        foreach ($categories as $category) {
+	            $category_ids[] = $category->term_id;
+	        }
+			
 		$tax_query = array(
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'product_category',
-                    'field' => 'id',
-                    'terms' => $category_ids,
-                    'operator' => 'IN'
-                )
-            ),
-            'post__not_in' => array($post_id)
-        );
-		
-		 $query->set( 'tax_query', $tax_query );
+	            'tax_query' => array(
+	                array(
+	                    'taxonomy' => 'product_category',
+	                    'field' => 'id',
+	                    'terms' => $category_ids,
+	                    'operator' => 'IN'
+	                )
+	            ),
+	        );
+			
+		$query->set( 'post__not_in', [$post_id] );
+		$query->set( 'tax_query', $tax_query );
 	}
 		
 } );
